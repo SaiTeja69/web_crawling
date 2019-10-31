8bf52c12c7574baf14abb9c38ec90e9ac9a9f3a1
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2019-10-31 21:02:05 +0530

    Update final.py

--- final.py
+++ final.py
@@ -5,8 +5,6 @@ header = {
   "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
   "X-Requested-With": "XMLHttpRequest"
 }
-
-
 url_main='https://oriparts.com/42/344/2746/753011'
 response = requests.get(url_main)
 screp = BeautifulSoup(response.text, 'html.parser')
@@ -15,7 +13,6 @@ websites=[]
 for i in range(4,len(screp.findAll('a'))):
     tag = screp.findAll('a')[i]
     websites.append(tag['href'])
-import pandas as pd
 df = pd.DataFrame(websites, columns=["colummn"])
 df.to_csv('websites.csv', index=False)
 k=1
